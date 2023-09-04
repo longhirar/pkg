84149e05f842a519dab33aeaf0afadd3e6a0c089
Rafael Longhi Varella Guimaraes <rafael.longhi@tecpuc.com.br>
2023-09-04 10:14:40 -0300

    add packagelist, initial lpm client and java17/8 placeholders

---
+++ lpm.py
@@ -0,0 +1,18 @@
+import os
+import sys
+import csv
+
+PKGLIST_URL = "https://pkg.longhi.me/packagelist"
+
+print("Baixando Lista de Pacotes...")
+if (os.system(f"powershell.exe Invoke-WebRequest -Uri {PKGLIST_URL} -OutFile C:\\Users\\{os.getlogin()}\\longhipkg\\packagelist.csv")) != 0:
+    print("===================================")
+    print("Falha ao atualizar lista de pacotes")
+    print("===================================")
+    sys.exit(1)
+
+
+with open(f"C:\\Users\\{os.getlogin()}\\longhipkg\\packagelist.csv", "r") as packagelistfile:
+    packagelist = csv.reader(packagelistfile, delimiter=";")
+    for package in packagelist:
+        print(package)
